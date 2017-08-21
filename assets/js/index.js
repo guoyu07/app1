ae168e33b5928bd8033add94b798dd1e61d6155d
doubleJ <673141651@qq.com>
2017-08-21 14:15:28 +0800

    轮播部分完成，搜索无选项卡

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,33 @@ var index = 0,
     len = $('.show li').length,
     timer = 0;
 
-timer = setInterval(function() {
+
+// 封装轮播效果函数
+function slider(cb) {
+    $('.show li').stop(true, true);
     $('.show li').eq(index).fadeOut(1000);
     $('.side-slider li').eq(index).removeClass('active');
-    index = ++index > len - 1 ? 0 : index;
+    cb();
     $('.show li').eq(index).fadeIn(1000);
     $('.side-slider li').eq(index).addClass('active');
-    console.log(index);
-}, 5000)
+}
+
+
+// 轮播图初始化调用
+function run() {
+    timer = setInterval(function() {
+        slider(function() {
+            index = ++index > len - 1 ? 0 : index;
+        })
+    }, 5000);
+}
+run();
+
+
+// 轮播点击特效
+$('.side-slider li').click(function() {
+    var that = $(this);
+    slider(function() {
+        index = that.index();
+    });
+})
